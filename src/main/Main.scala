fdd5a2424713f14d0470ef92940c2fca6583a2a6
Jaakko Paju <jaakko.paju2@gmail.com>
2020-01-31 13:12:16 +0200

    Added fold method to Tree.

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,9 +1,15 @@
+import introduction.{Branch, Leaf, Tree}
+
 object Main {
 	def main(args: Array[String]): Unit = {
 		val intList = introduction.List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 		val doubleList = introduction.List(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
 
-		val x = introduction.List.zipWith(introduction.List(1, 2, 3), introduction.List(3, 2, 1))(_ + _)
+		val intTree = Branch(
+			Branch(Leaf(4), Leaf(3)), Branch(Leaf(2), Branch(Leaf(1), Leaf(5)))
+		)
+
+		val x = Tree.map(intTree)(_ + 5)
 		println(x)
 	}
 
--- src/main/introduction/Tree.scala
+++ src/main/introduction/Tree.scala
@@ -7,7 +7,7 @@ case class Leaf[A](value: A) extends Tree[A]
 object Tree {
 
 	def size[A](t: Tree[A]): Int = t match {
-		case Branch(left, right) => 1 + size(left) + size(right)
+		case Branch(l, r) => 1 + size(l) + size(r)
 		case Leaf(_) => 1
 	}
 
@@ -21,10 +21,25 @@ object Tree {
 		case Leaf(_) => 1
 	}
 
-	def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+	def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
 		case Branch(l, r) => Branch(map(l)(f), map(r)(f))
-		case Leaf(value) => Leaf(f(value))
+		case Leaf(v) => Leaf(f(v))
 	}
 
-	def fold[A,B](t: Tree[A], id: B)(f: (A,B) => B): Tree[B] = ???
+	def fold[A,B](t: Tree[A])(f: A => B)(combine: (B,B) => B): B = t match {
+		case Branch(l, r) => combine(fold(l)(f)(combine), fold(r)(f)(combine))
+		case Leaf(v) => f(v)
+	}
+
+	def size2[A](t: Tree[A]): Int =
+		fold(t)(_ => 1)(1 + _ + _)
+
+	def maximum2(t: Tree[Int]): Int =
+		fold(t)(identity)(_ max _)
+
+	def depth2[A](t: Tree[A]): Int =
+		fold(t)(_ => 1)((ld,rd) => 1 + (ld max rd))
+
+	def map2[A,B](t: Tree[A])(f: A => B): Tree[B] =
+		fold(t)(v => Leaf(f(v)): Tree[B])(Branch(_,_))
 }
